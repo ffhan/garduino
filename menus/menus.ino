e74c518ce7898b2ba167ef4c252d87c15fab2da9
Fran Hančić <fhancic@gmail.com>
2018-03-02 02:18:21 +0100

    Screen show, huge amount of bugfixing
    Code should now be bug-free, at least the things I'm currently using.
    
    Show() method in Screen currently only prints to Serial, but does so the same way lcd.print would. The only thing I have to do is introduce a delegate that would print wherever it has to print. (multiple serials, lcd, etc.).
    
    Next step is implementing Settings. That requires Action, which could be developed completely separately.

--- menus/menus.ino
+++ menus/menus.ino
@@ -101,7 +101,7 @@ class ItemNode{
   public : 
   ItemNode *previous = NULL;
   ItemNode *next = NULL;
-  Item *item;
+  Item *item = NULL;
 };
 /*
  * a better name would be SubMenu, but I didn't want to draw a too close relationship to
@@ -112,15 +112,13 @@ class ItemNode{
 class ItemList{
   public:
   int len = 0;
-  ItemNode *head;
-  ItemNode *tail;
+  ItemNode *head = NULL;
+  ItemNode *tail = NULL;
 
   Item *parent;
 
   ItemList(Item *parent){
     this->len = 0;
-    this->head = NULL;
-    this->tail = NULL;
     this->parent = parent;
   }
 
@@ -157,24 +155,23 @@ class ItemList{
       Serial.println("empty.");
       return NULL;
     }
+    else if(index >= len) return NULL;
+    
     ItemNode *head = this->head;
-    Item *item = NULL;
-    int i = 0;
-    while(head && i <= index){
-      item = head->item;
+    Item *item = head->item;
+    int i = 1;
+    while(head && i <= index && item){
       head = head->next;
+      item = head->item;
       i++;
     }
-    if(item) return item;
-    else{
-      Serial.println(F("failed."));
-      return;
-    }
+    if(!head && i > index) item = NULL;
+    
+    return item;
   }
   void process(int index, Printer func){
     Item *item = get(index);
     if(item) func(item);
-    else Serial.println(F("failed."));
   }
   void processAll(Printer func){
     ItemNode *head = this->head;
@@ -254,10 +251,14 @@ void printItem(Item *item){
 }
 
 
+
 class Screen{
   
   private : 
-  
+
+  static const char space = ' ';
+  static const char slct = '>';
+  static const char menuSign = '+';
   
   byte rows, cols;
   Menu *mainMenu;
@@ -271,12 +272,8 @@ class Screen{
   byte *cursorPosition, index = 0, mainPosition = 0;
 
   public : 
-  
-  typedef enum{
-    POINT = 62
-  } symbols;
 
-  Screen(byte rows, byte cols, Menu *mainMenu){
+  Screen(byte cols, byte rows, Menu *mainMenu){
     this->rows = rows;
     this->cols = cols;
     bindToMenu(mainMenu);
@@ -296,7 +293,9 @@ class Screen{
     currentItem = currentItem->up();
   }
   void down(){
-    if(currentItem == currentItem->down()) return;
+    if(currentItem == currentItem->down()){
+      return;
+    }
     index++;
     if(*cursorPosition + 1 < rows) (*cursorPosition)++;
     currentItem = currentItem->down();
@@ -305,14 +304,14 @@ class Screen{
     if(currentItem == currentItem->back()) return;
     Menu *menu = mainMenu->back();
     //Serial.print(mainMenu->getTitle());Serial.print(" ");
-    if(mainMenu != menu){
+    if(mainMenu != menu && menu){
       mainMenu = menu;
       cursorPosition = mainMenu->getSelector();
+      currentItem = currentItem->back();
+      index = currentItem->getIndex();
     }
-    else cursorPosition = &mainPosition; // bind it to a default value 0.
+
     //Serial.println(mainMenu->getTitle());
-    currentItem = currentItem->back();
-    index = currentItem->getIndex();
   }
   void enter(){
     if(currentItem->getType() != MENU) return; //expand later.
@@ -322,14 +321,85 @@ class Screen{
     if(currentItem == menu->enter()) return;
     mainMenu = menu;
     cursorPosition = mainMenu->getSelector();
-    //*cursorPosition = 0; // reset the cursor position, because we do the same 
-    //for the index.
+    *cursorPosition = 0; // reset the cursor position, because we do the same for the index.
     currentItem = mainMenu->enter();
     index = currentItem->getIndex();
   }
   void flash(Printer func){
-    Serial.print(*cursorPosition); Serial.print(" "); Serial.println(index);
+    Serial.print(*cursorPosition); Serial.print(space); Serial.println(index);
     func(currentItem);
+  }
+  void show(){
+    Serial.print(*cursorPosition); Serial.print(space); Serial.print(index);
+    Serial.print(space);Serial.println(mainMenu->getTitle());
+    /*
+     * Screen is set up this way:
+     * Each row has 3 parts:
+     * _SLCT_|_________Title__________|_marker_
+     *    >  |     Level setting      |  .xX
+     *       |   Numerical setting    |   6
+     *       |  Boolean false setting |   o
+     *       |  Boolean true setting  |   x
+     *       |    Simple separator    |
+     *       |       Menu example     |   +
+     *       
+     * SLCT is a part that determines if the current Item is selected. It takes up 1 column.
+     * Marker is a part that gives additional info on the type of an Item and its' 
+     * state (if it's a Setting). It holds 3 columns.
+     * Title defines what a specific Item represents. It holds (by default) 12 columns.
+     * I'll maybe enable restructuring user interface (column size, etc.) in later stages.
+     */
+    Item *item;
+    byte processed = 0;
+    byte titleCols = cols - 4;
+    Serial.println(F("----------------"));
+    for(byte i = index - *cursorPosition; i < index - *cursorPosition + rows; i++){
+      
+      item = mainMenu->getSubMenuItem(i);
+      if(item){
+        if(processed == *cursorPosition){
+          Serial.print(slct);
+        }
+        else{
+          Serial.print(space);
+        }
+        processed++;
+        if(strlen(item->getTitle()) >= titleCols){
+          for(byte j = 0; j < titleCols; j++){
+            Serial.print(item->getTitle()[j]);
+          }
+        }
+        else{
+          for(byte j = 0; j < (titleCols - strlen(item->getTitle())) / 2; j++){
+            Serial.print(space);
+          }
+          Serial.print(item->getTitle());
+          for(byte j = 0; j < (titleCols - strlen(item->getTitle())) / 2 + 
+          (titleCols - strlen(item->getTitle())) % 2; j++){
+            Serial.print(space);
+          }
+        }
+        switch(item->getType()){
+          case MENU:
+          Serial.print(space);
+          Serial.print(menuSign);
+          Serial.print(space);
+          break;
+          case ITEM:
+          for(byte j = 0; j < 3; j++){
+            Serial.print(space);
+          }
+          break;
+        }
+      }
+      else{
+        for(byte j = 0; j < cols; j++){
+          Serial.print(space);
+        }
+      }
+      Serial.println();
+    }
+    Serial.println(F("----------------"));
   }
 };
 
@@ -393,7 +463,8 @@ void loop() {
         randomMenu.processChildItems(&printItem);
         break;
       }
-    screen->flash(&printItem);
+    //screen->flash(&printItem);
+    screen->show();
     typed = true;
     }
   }
